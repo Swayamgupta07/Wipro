b4e185856e8a8008d5ebb4ccaf9aac8beee934f4
Swayam Gupta <swayamgupta09@gmail.com>
2026-05-23 12:25:54 +0530

    Adding Assignment 16

---
+++ Assignment_16/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Student } from './student/student';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Footer, Student],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Assignment_16');
+}
