5221fb806c3514975751a7aef91dd17ce68bffd9
Swayam Gupta <swayamgupta09@gmail.com>
2026-05-29 15:17:45 +0530

    Renaming and correction names

--- Assignment_19_day_25_EmployeeCRUD_JSONServer/src/app/app.routes.ts
+++ Assignment_19_day_25_EmployeeCRUD_JSONServer/src/app/app.routes.ts
@@ -4,18 +4,9 @@ import { EmployeeForm } from './components/employee-form/employee-form';
 import { EmployeeDetails } from './components/employee-details/employee-details';
 
 export const routes: Routes = [
-  // 1. Root route: Display the central employee list
   { path: '', component: EmployeeList },
-  
-  // 2. Add route: Render the form to register a new employee
   { path: 'add', component: EmployeeForm },
-  
-  // 3. Edit route: Render the form populated with existing data using the employee ID
   { path: 'edit/:id', component: EmployeeForm },
-  
-  // 4. Details route: Display a detailed visual card of the employee profile
   { path: 'details/:id', component: EmployeeDetails },
-  
-  // 5. Wildcard fallback: Redirect any invalid/broken URLs back to the root dashboard
   { path: '**', redirectTo: '' }
 ];
